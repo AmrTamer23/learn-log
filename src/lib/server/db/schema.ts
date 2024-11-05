33b18bacdf3870d5b01b567e46c5946ae2c1e2f1
AmrTamer23 <amrtamer23mj@gmail.com>
2024-11-05 05:55:28 +0200

    feat(backend): implement schema

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,39 @@
+import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
+
+export const User = sqliteTable('User', {
+	id: integer('id')
+		.primaryKey({
+			autoIncrement: true
+		})
+		.notNull(),
+	name: text('name').notNull(),
+	password: text('password').notNull(),
+	dob: text('dob')
+});
+
+export const Course = sqliteTable('Course', {
+	id: integer('id')
+		.primaryKey({
+			autoIncrement: true
+		})
+		.notNull(),
+	name: text('name').notNull(),
+	desc: text('desc'),
+	dof: text('dof'),
+	color: text('color'),
+	emoji: text('emoji'),
+	progress: integer('progress')
+});
+
+export const Task = sqliteTable('Task', {
+	id: integer('id')
+		.primaryKey({
+			autoIncrement: true
+		})
+		.notNull(),
+	title: text('title').notNull(),
+	desc: text('desc'),
+	isDone: integer('isDone', {
+		mode: 'boolean'
+	})
+});
