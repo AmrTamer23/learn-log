1689f461914c96072073571858885d67723fed4c
AmrTamer23 <amrtamer23mj@gmail.com>
2024-11-11 20:14:00 +0200

    chore: minor changes on navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -12,7 +12,7 @@
 	<div>
 		<a
 			href="/"
-			class="flex items-center font-highlight text-4xl font-semibold text-[#f9e8a4] !no-underline"
+			class="flex items-center font-highlight text-4xl font-semibold text-primary !no-underline"
 		>
 			Learn Log
 		</a>
