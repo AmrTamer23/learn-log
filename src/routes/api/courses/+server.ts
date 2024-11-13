6c651cc66c513f2dfc5b979e8055812d0671515f
AmrTamer23 <amrtamer23mj@gmail.com>
2024-11-13 04:21:38 +0200

    feat(backend): post handler for courses route to create course

--- src/routes/api/courses/+server.ts
+++ src/routes/api/courses/+server.ts
@@ -25,3 +25,43 @@ export const GET: RequestHandler = async ({ request, cookies }) => {
 
 	return json({ courses });
 };
+
+export const POST: RequestHandler = async ({ request, cookies }) => {
+	const token = cookies.get('auth_token');
+
+	if (!token) {
+		return error(401, 'Unauthorized');
+	}
+
+	const user = jwt.decode(token) as { userId: string; email: string };
+
+	if (!user) {
+		return error(401, 'Unauthorized');
+	}
+
+	const { name, description, dateOfStart, color, emoji, instructor } = await request.json();
+
+	if (!name || !description || !dateOfStart || !color) {
+		return error(400, 'Missing required fields');
+	}
+
+	if (name.length < 2 || name.length > 50) {
+		return error(400, 'Name must be between 2 and 50 characters');
+	}
+
+	if (description.length > 500) {
+		return error(400, 'Description must be less than 500 characters');
+	}
+
+	const courses = await db.insert(course).values({
+		userId: parseInt(user.userId),
+		name,
+		description,
+		dateOfStart,
+		color,
+		emoji,
+		instructor
+	});
+
+	return json({ courses });
+};
